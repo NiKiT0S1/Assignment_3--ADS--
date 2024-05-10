c33ac6b3ec6f08600a8970c59e573f95673d519a
NiKiT0S1 <bobilevnikita88@gmail.com>
2024-05-10 17:30:00 +0500

    Add data

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
 
-    }
+        binaryTree.insert(5);
+        binaryTree.insert(3);
+        binaryTree.insert(4);
+        binaryTree.insert(2);
+        binaryTree.insert(6);
+        binaryTree.insert(7);
 
-    public Main() {
-
+        binaryTree.inOrder();
+        System.out.println();
+        System.out.println("Root data before remove: " + binaryTree.root.data);
+        binaryTree.remove(5);
+        System.out.println();
+        binaryTree.inOrder();
+        System.out.println();
+        System.out.println("Root data after remove " + binaryTree.root.data);
     }
 }
