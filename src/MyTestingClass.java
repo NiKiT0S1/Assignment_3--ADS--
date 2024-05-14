f4709417f6a28624f97c8711cf859f89bb74540b
NiKiT0S1 <bobilevnikita88@gmail.com>
2024-05-14 16:10:24 +0500

    Add methods to MyTestingClass

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,11 +1,13 @@
-import java.util.Random;
+//import java.util.Random;
 
 public class MyTestingClass {
     private int id;
-    private static final Random RANDOM = new Random();
+    //private static final Random RANDOM = new Random();
+    private String data;
 
-    public MyTestingClass(int id) {
+    public MyTestingClass(int id, String data) {
         this.id = id;
+        this.data = data;
     }
 
     @Override
@@ -13,6 +15,7 @@ public class MyTestingClass {
         // Простой пример пользовательского метода hashCode
         int hash = 7;
         hash = 31 * hash + id;
+        hash = 31 * hash + (data == null ? 0 : data.hashCode());
         return hash;
     }
 
@@ -21,30 +24,16 @@ public class MyTestingClass {
         return id;
     }
 
+    public String getData() {
+        return data;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
 
-    public static void main(String[] args) {
-        MyHashTable<MyTestingClass, String> table = new MyHashTable<>();
-        int buckets = 11; // Количество бакетов в хеш-таблице
-        int[] bucketSizes = new int[buckets];
-
-        // Добавление 10000 случайных элементов в хеш-таблицу
-        for (int i = 0; i < 10000; i++) {
-            MyTestingClass key = new MyTestingClass(RANDOM.nextInt(10000));
-            String value = "Student" + i;
-            table.put(key, value);
-
-            // Увеличение размера соответствующего бакета
-            int index = key.hashCode() % buckets;
-            bucketSizes[index]++;
-        }
-
-        // Вывод количества элементов в каждом бакете
-        for (int i = 0; i < buckets; i++) {
-            System.out.println("Bucket " + i + ": " + bucketSizes[i] + " elements");
-        }
+    public void setData(String data) {
+        this.data = data;
     }
 }
 
